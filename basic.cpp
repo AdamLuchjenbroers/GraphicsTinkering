e4e79eb89e6bc9857420ded4f29de507e7009589
AdamLuchjenbroers <adam.luchjenbroers@gmail.com>
2014-11-21 21:41:28 +1100

    Forgot the buffer swap...

--- basic.cpp
+++ basic.cpp
@@ -6,19 +6,21 @@
 int main( int argc, char* args[] ) { 
   SDL_Window *app_window;
   SDL_GLContext app_glcontext;
-  GLfloat color[] = {0.0f, 0.0f, 1.0f, 1.0f};
 
   //Start SDL 
   SDL_Init( SDL_INIT_EVERYTHING ); 
 
   app_window = SDL_CreateWindow("I'm a window", 50,50,200,200, SDL_WINDOW_OPENGL);
   app_glcontext = SDL_GL_CreateContext(app_window);  
+  SDL_GL_MakeCurrent(app_window, app_glcontext);
 
   printf("GL Version: %s\n",  glGetString(GL_VERSION));
   printf("GL Renderer: %s\n", glGetString(GL_RENDERER));
   printf("GL Vendor: %s\n", glGetString(GL_VENDOR));
 
+  glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
   glClear(GL_COLOR_BUFFER_BIT);
+  SDL_GL_SwapWindow(app_window);
 
   SDL_Delay(2000);
  

12c3028a2d935d9320e05ef7fa32ac30cfb122c9
AdamLuchjenbroers <adam.luchjenbroers@gmail.com>
2014-11-21 19:55:59 +1100

    Print OpenGL renderer info for convenience

--- basic.cpp
+++ basic.cpp
@@ -1,15 +1,23 @@
+
+#include <stdio.h>
 #include "SDL2/SDL.h" 
 #include "GL/gl.h"
 
 int main( int argc, char* args[] ) { 
   SDL_Window *app_window;
+  SDL_GLContext app_glcontext;
   GLfloat color[] = {0.0f, 0.0f, 1.0f, 1.0f};
 
   //Start SDL 
   SDL_Init( SDL_INIT_EVERYTHING ); 
 
   app_window = SDL_CreateWindow("I'm a window", 50,50,200,200, SDL_WINDOW_OPENGL);
-  
+  app_glcontext = SDL_GL_CreateContext(app_window);  
+
+  printf("GL Version: %s\n",  glGetString(GL_VERSION));
+  printf("GL Renderer: %s\n", glGetString(GL_RENDERER));
+  printf("GL Vendor: %s\n", glGetString(GL_VENDOR));
+
   glClear(GL_COLOR_BUFFER_BIT);
 
   SDL_Delay(2000);

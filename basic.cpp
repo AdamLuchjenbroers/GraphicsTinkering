e1f9211fc591d88988f29ff0d6862c8bd6c9d6fc
AdamLuchjenbroers <adam.luchjenbroers@gmail.com>
2014-11-21 19:45:45 +1100

    Clear colour buffer

--- basic.cpp
+++ basic.cpp
@@ -1,13 +1,17 @@
 #include "SDL2/SDL.h" 
+#include "GL/gl.h"
 
 int main( int argc, char* args[] ) { 
   SDL_Window *app_window;
+  GLfloat color[] = {0.0f, 0.0f, 1.0f, 1.0f};
 
   //Start SDL 
   SDL_Init( SDL_INIT_EVERYTHING ); 
 
   app_window = SDL_CreateWindow("I'm a window", 50,50,200,200, SDL_WINDOW_OPENGL);
   
+  glClear(GL_COLOR_BUFFER_BIT);
+
   SDL_Delay(2000);
  
   SDL_DestroyWindow(app_window);

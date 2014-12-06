be5e23d7e7a9922bd696fa534e6b3790b12a8199
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2014-12-06 11:03:41 +1100

    Add Doxygen comments to ShaderLibrary.h

--- fw1/ShaderLibrary.h
+++ fw1/ShaderLibrary.h
@@ -4,11 +4,29 @@
 
 #include <map>
 
+/** Manages a collection of shader script files and supplies the shader as requested. It expects the shader library
+ *  to be organized with sub-folders for each GLSL version and will select the shader script file corresponding to
+ *  the highest GLSL version supported by the current rendering context.
+ *
+ *  This class uses a singleton design patten.
+ */
 class ShaderLibrary {
 public:
+/** Retrieve a Shader object corresponding to the script name provided, selecting a version of the shader that
+ *  is supported by the current rendering context.
+ *  @param name The filename of the shader script (without directory)
+ *  @param stage The GL pipeline stage for this shader.
+ *  @return A Shader object created with the requested script and for the requested pipeline stage.
+ */
     Shader *getShader(const char *name, GLuint stage);
 
+/** Gets the Shader Library object, or creates it if required using the default shader search path.
+ *  @return A pointer to the shader library.
+ */
     static ShaderLibrary *getLibrary();
+/** Replaces the current Shader Library object with a new Shader Library based in the specified path
+ *  @param newpath The new base search path for the shader library.
+ */
     static void setLibraryPath(char *newpath);
 private:
     static ShaderLibrary *library;

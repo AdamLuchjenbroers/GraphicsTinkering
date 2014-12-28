8af1184b041cadadbf941f7fd1e96e6db1df5c84
AdamLuchjenbroers <adam.luchjenbroers@gmail.com>
2014-12-28 14:47:21 +0930

    Emit the names of successful test cases

--- tests.cpp
+++ tests.cpp
@@ -1,4 +1,7 @@
 #include <cppunit/TestSuite.h>
+#include <cppunit/TestResult.h>
+#include <cppunit/TestResultCollector.h>
+#include <cppunit/BriefTestProgressListener.h>
 #include <cppunit/ui/text/TestRunner.h>
 
 #include "fw1/tests.h"
@@ -6,11 +9,17 @@
 
 int main() {
     CppUnit::TextUi::TestRunner runner;
+    CppUnit::TestResult controller;
+    CppUnit::TestResultCollector collector;
+    CppUnit::BriefTestProgressListener progress;
+
+    controller.addListener(&collector);
+    controller.addListener(&progress);
 
     runner.addTest(FrameworkOneTestSuite::suite());
     runner.addTest(MathTestSuite::suite());
 
-    runner.run();
+    runner.run(controller);
 
     return 0;
 }

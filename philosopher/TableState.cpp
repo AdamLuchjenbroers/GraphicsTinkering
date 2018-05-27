8426f4e8fda9be6da98eb08eeca2eb96f616a8a6
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2018-05-27 18:36:47 +1000

    Refactoring: TableState now writes to a provided buffer instead of maintaining its own

--- philosopher/TableState.cpp
+++ philosopher/TableState.cpp
@@ -1,15 +1,20 @@
 
 #include "TableState.h"
+#include <stdio.h>
 
 TableState::TableState(int diners) {
   _diners = diners;
-
-  philosophers = new PhilosopherState[diners];
-  chopsticks = new ChopstickState[diners];
 }
+
 
 TableState::~TableState() {
-  delete philosophers;
-  delete chopsticks;
 }
 
+void TableState::write_state(GLvoid *target) {
+  GLint *buf = (GLint *)target;
+
+  for(int i = 0;i < _diners;i++) {
+    buf[2*i]     = TableState::PHILOSOPHER_THINKING; 
+    buf[(2*i)+1] = TableState::CHOPSTICK_FREE; 
+  }
+}
--- philosopher/TableState.h
+++ philosopher/TableState.h
@@ -1,27 +1,32 @@
 #pragma once
 
+#include "../fw1/fw1.h"
+
 class TableState {
 public: 
   TableState(int diners);
   ~TableState();
 
-  enum PhilosopherState {
-      PHILOSOPHER_THINKING
-  ,   PHILOSOPHER_WAITING
-  ,   PHILOSOPHER_EATING
+  // Use explicit assignment here and in GLSL
+  // so we can easily ensure / verify alignment.
+  enum ItemState {
+      PHILOSOPHER_THINKING = 0
+  ,   PHILOSOPHER_WAITING  = 1
+  ,   PHILOSOPHER_EATING   = 2
+  ,   CHOPSTICK_FREE       = 4
+  ,   CHOPSTICK_LEFT       = 5
+  ,   CHOPSTICK_RIGHT      = 6
   };
 
-  enum ChopstickState {
-      CHOPSTICK_FREE
-  ,   CHOPSTICK_LEFT
-  ,   CHOPSTICK_RIGHT
+  enum ItemType {
+      PHILOSOPHER = 1
+  ,   CHOPSTICK   = 2
   };
 
   int num_diners() { return _diners; };
 
+  void write_state(GLvoid *target);
+
 private:
   int _diners;
-
-  PhilosopherState *philosophers;
-  ChopstickState *chopsticks;
 };

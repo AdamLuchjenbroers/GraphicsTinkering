70d16e380dcf2e6dcd359cd14e4a323f0a1bdc97
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2018-06-07 09:35:45 +1000

    NaiveTable - Use PhilosopherRef and load in Philosopher objects as needed

--- philosopher/NaiveTable.cpp
+++ philosopher/NaiveTable.cpp
@@ -5,6 +5,7 @@ NaiveTable::NaiveTable(int diners)
 : TableState(diners) {
 
   for(int i=0;i<_diners;i++) {
-     _philosophers[i].setup(this, i);
+     Philosopher *p = new Philosopher(this, i);
+     _philosophers[i].set_reference(p);
   }
 }

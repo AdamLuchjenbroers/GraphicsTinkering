48433b61f531d1f7bdc2bfb06cb28c90d78eedc9
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2018-06-10 12:40:33 +1000

    Add mutexes for holding / accessing chopsticks

--- philosopher/Chopstick.cpp
+++ philosopher/Chopstick.cpp
@@ -2,14 +2,44 @@
 
 Chopstick::Chopstick() {
   pthread_mutex_init( &_mtx_held, NULL );
+  pthread_mutex_init( &_mtx_access, NULL );
 
-  _state = ItemState::CHOPSTICK_LEFT;
+  _state = ItemState::CHOPSTICK_FREE;
 }
 
 Chopstick::~Chopstick() {
   pthread_mutex_destroy( &_mtx_held );
+  pthread_mutex_destroy( &_mtx_access );
 }
 
 ItemState Chopstick::get_state() {
-  return _state;
+  ItemState state;
+
+  pthread_mutex_lock(&_mtx_access);
+  state = _state;
+  pthread_mutex_unlock(&_mtx_access);
+
+  return state;
 }
+
+void Chopstick::grab(bool left) {
+  pthread_mutex_lock(&_mtx_access);
+  
+  pthread_mutex_lock(&_mtx_held);
+
+  if (left) {
+    _state = ItemState::CHOPSTICK_LEFT;
+  } else {
+    _state = ItemState::CHOPSTICK_RIGHT;
+  }
+  pthread_mutex_unlock(&_mtx_access);
+}
+
+void Chopstick::release() {
+  pthread_mutex_lock(&_mtx_access);
+  
+  _state = ItemState::CHOPSTICK_FREE;
+
+  pthread_mutex_unlock(&_mtx_held);
+  pthread_mutex_unlock(&_mtx_access);
+} 
--- philosopher/Chopstick.h
+++ philosopher/Chopstick.h
@@ -9,7 +9,23 @@ public:
 
   ItemState get_state();
 
+  void grab(bool left);
+  void release();
+
 protected:
+  /* Two Mutexes are used to control access to Chopstick:
+
+     _mtx_access is used when reading or writing state. It should be held only 
+        while performing and read/write operations on current state.
+
+     _mtx_held is held by the philosopher currently holding the chopstick. and
+        is held for the duration of the chopstick being held.
+
+     Locks should always be acquired in the order: access, held.
+  */
+
   pthread_mutex_t _mtx_held;
+  pthread_mutex_t _mtx_access;
+
   ItemState _state;
 };
